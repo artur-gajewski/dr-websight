6ba82ce1f28dbea4d108ab44ea0d97fdfcd2a6be
Kalle Lamminpää <lamminpaakm@gmail.com>
2015-12-10 00:54:26 +0200

    initial server

--- server/server.js
+++ server/server.js
@@ -2,22 +2,13 @@ var restify = require('restify');
 var request = require('request');
 
 var server = restify.createServer();
-server.post('/ping', function create(req, res, next) {
-    request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    res.send(200, {"status":response.statusCode, "description": "OK", "url":req.body.url});
-   return next();
-  }
-    })
-   
- });
 
-server.get('/ping', function create(req, res, next) {
-    request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    res.send(200, {"status":response.statusCode, "description": "OK", "url":"http://www.google.com"});
-   return next();
-  }
+server.post('/ping', function create(req, res, next) {
+    request(req.body.url, function (error, response, body) {
+      if (!error) {
+        res.send(200, {"status":response.statusCode, "description": "OK", "url":req.body.url});
+        return next();
+      }
     })
    
  });
