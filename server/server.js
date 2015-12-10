f14fb5336f5e5d770430ae0bc41b2a617056258a
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2015-12-10 10:16:51 +0200

    Fixed issues with body parsing

--- server/server.js
+++ server/server.js
@@ -3,10 +3,19 @@ var request = require('request');
 
 var server = restify.createServer();
 
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
 server.post('/ping', function create(req, res, next) {
     request(req.body.url, function (error, response, body) {
+
       if (!error) {
-        res.send(200, {"status":response.statusCode, "description": "OK", "url":req.body.url});
+        res.send(200, {
+            "status": response.statusCode,
+            "description": "OK",
+            "url":req.body.url
+        });
         return next();
       }
     })
@@ -16,3 +25,5 @@ server.post('/ping', function create(req, res, next) {
 server.listen(1235, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+
