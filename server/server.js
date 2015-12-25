52f3531d623713573f57969f82d0f0dcc4741b13
Kalle Lamminpää <lamminpaakm@gmail.com>
2015-12-25 15:12:53 +0200

    fixed intendation

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 var restify = require('restify');
 var request = require('request');
 var pg = require('pg');
+var config = require('config');
 
 var server = restify.createServer();
 
-var conString = "postgres://postgres:mysecretpassword@192.168.99.100/postgres";
+var dbConfig = config.get('Server.dbConfig');
+var conString = "postgres://"+dbConfig.user+":"+dbConfig.password+"@"+dbConfig.host+"/"+dbConfig.dbName;
+
 var client = new pg.Client(conString);
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -13,46 +16,47 @@ server.use(restify.bodyParser());
 server.use(restify.CORS());
 server.use(restify.fullResponse());
 
+var handleError = function(err) {
+  // no error occurred, continue with the request
+  if(!err) return false;
 
+  if(client){
+    done(client);
+  }
+  res.send(500, {
+    "code": "500",
+    "description": err,
+    "url": req.body.url
+  });
+  return true;
+};
 
 server.post('/ping', function create(req, res, next) {
-    request(req.body.url, function (error, response, body) {
-        console.log("Pinging: " + req.body.url);
-        if (!error) {
-            res.setHeader('Content-Type', 'application/json');
-            console.log("Got response from " + req.body.url);
-            res.send(200, {
-                "code": response.statusCode,
-                "description": "OK",
-                "url": req.body.url
-            });
-            return next();
-        } else {
-            res.send(200, {
-                "code": "DNS-3",
-                "description": "Not Found",
-                "url": req.body.url
-            });
-            return next();
-        }
-    })
+  request(req.body.url, function (error, response, body) {
+    console.log("Pinging: " + req.body.url);
+    if (!error) {
+      res.setHeader('Content-Type', 'application/json');
+      console.log("Got response from " + req.body.url);
+      res.send(200, {
+        "code": response.statusCode,
+        "description": "OK",
+        "url": req.body.url
+      });
+      return next();
+    } else {
+      res.send(200, {
+        "code": "DNS-3",
+        "description": "Not Found",
+        "url": req.body.url
+      });
+      return next();
+    }
+  })
  });
 
 server.post('/url', function create(req, res, next) {
      // get a pg client from the connection pool
   pg.connect(conString, function(err, client, done) {
-
-    var handleError = function(err) {
-      // no error occurred, continue with the request
-      if(!err) return false;
-
-      if(client){
-        done(client);
-      }
-      res.writeHead(500, {'content-type': 'text/plain'});
-      res.end('An error occurred');
-      return true;
-    };
 
     // handle an error from the connection
     if(handleError(err)) return;
@@ -63,9 +67,9 @@ server.post('/url', function create(req, res, next) {
       // handle an error from the query
       if(handleError(err)) return;
       res.send(200, {
-                "code": "200",
-                "description": "OK",
-                "url": req.body.url
+        "code": "200",
+        "description": "OK",
+        "url": req.body.url
       });
       done();
     });
@@ -73,19 +77,7 @@ server.post('/url', function create(req, res, next) {
 });
 
 server.get('/urls', function create (req, res, next) {
-    pg.connect(conString, function(err, client, done) {
-
-    var handleError = function(err) {
-      // no error occurred, continue with the request
-      if(!err) return false;
-
-      if(client){
-        done(client);
-      }
-      res.writeHead(500, {'content-type': 'text/plain'});
-      res.end('An error occurred');
-      return true;
-    };
+  pg.connect(conString, function(err, client, done) {
 
     // handle an error from the connection
     if(handleError(err)) return;
@@ -94,36 +86,21 @@ server.get('/urls', function create (req, res, next) {
     // get the total number of visits today (including the current visit)
     client.query('SELECT * FROM uri', function(err, result) {
 
-        // handle an error from the query
-        if(handleError(err)) return;
+      // handle an error from the query
+      if(handleError(err)) return;
 
-        // return the client to the connection pool for other requests to reuse
-        done();
-        var rows = result.rows;
-        res.send(200, {
-               rows
+      // return the client to the connection pool for other requests to reuse
+      done();
+      var rows = result.rows;
+      res.send(200, {
+             rows
       });
     });
   });
 });
 
 server.del('/url/:uuid', function rm (req, res, next) {
-    pg.connect(conString, function(err, client, done) {
-
-    var handleError = function(err) {
-      // no error occurred, continue with the request
-      if(!err) return false;
-
-      if(client){
-        done(client);
-      }
-      res.send(500, {
-                "code": "500",
-                "description": err,
-                "url": req.body.url
-      });
-      return true;
-    };
+  pg.connect(conString, function(err, client, done) {
 
     // handle an error from the connection
     if(handleError(err)) return;
@@ -132,37 +109,22 @@ server.del('/url/:uuid', function rm (req, res, next) {
     // get the total number of visits today (including the current visit)
     client.query('DELETE FROM uri WHERE id = $1',[req.params.uuid], function(err, result) {
 
-        // handle an error from the query
-        if(handleError(err)) return;
+      // handle an error from the query
+      if(handleError(err)) return;
 
-        // return the client to the connection pool for other requests to reuse
-        done();
-        res.send(200, {
-                "code": "200",
-                "description": "DEL OK",
-                "url": req.body.url
+      // return the client to the connection pool for other requests to reuse
+      done();
+      res.send(200, {
+        "code": "200",
+        "description": "DEL OK",
+        "url": req.body.url
       });
     });
   });
 });
 
 server.put('/url/:uuid', function rm (req, res, next) {
-    pg.connect(conString, function(err, client, done) {
-
-    var handleError = function(err) {
-      // no error occurred, continue with the request
-      if(!err) return false;
-
-      if(client){
-        done(client);
-      }
-      res.send(500, {
-                "code": "500",
-                "description": err,
-                "url": req.body.url
-      });
-      return true;
-    };
+  pg.connect(conString, function(err, client, done) {
 
     // handle an error from the connection
     if(handleError(err)) return;
@@ -171,15 +133,15 @@ server.put('/url/:uuid', function rm (req, res, next) {
     // get the total number of visits today (including the current visit)
     client.query('UPDATE uri SET uri=$2 WHERE id = $1',[req.params.uuid, req.body.url], function(err, result) {
 
-        // handle an error from the query
-        if(handleError(err)) return;
+      // handle an error from the query
+      if(handleError(err)) return;
 
-        // return the client to the connection pool for other requests to reuse
-        done();
-        res.send(200, {
-                "code": "200",
-                "description": "UPDATE OK",
-                "url": req.body.url
+      // return the client to the connection pool for other requests to reuse
+      done();
+      res.send(200, {
+        "code": "200",
+        "description": "UPDATE OK",
+        "url": req.body.url
       });
     });
   });
@@ -187,5 +149,5 @@ server.put('/url/:uuid', function rm (req, res, next) {
 
 
 server.listen(1235, function() {
-    console.log('%s listening at %s', server.name, server.url);
+  console.log('%s listening at %s', server.name, server.url);
 });
